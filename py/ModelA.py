4ec3f7dee65822eba6663cbc1ff563f36af31431
Connor Owen <ccowen37@gmail.com>
2020-11-08 10:33:05 -0500

    fix objective, demand constraints

--- py/ModelA.py
+++ py/ModelA.py
@@ -12,7 +12,8 @@ costs_df = pd.read_csv('../csv/DeliveryCost.csv')
 
 # {order id: product id: amount} only for product ids with amount > 0
 orders_df = orders_df.groupby(['Order ID', 'Product ID']).sum()
-orders_data = {i: {j: group.at[(i,j), 'Quantity'] for j in group.index.get_level_values(1)} for i, group in orders_df.groupby(level=0)}
+orders_data = {i: {j: group.at[(i,j), 'Quantity'] for j in group.index.get_level_values(1)}
+                                                    for i, group in orders_df.groupby(level=0)}
 
 # {product ID: weight}
 product_weights = {p: product_weight_df.at[p, 'Weight'] for p in product_weight_df.index}
@@ -23,7 +24,8 @@ costs = {i: {int(j[1]): group.at[j] for j in group.index} for i, group in costs.
 
 # {warehouse ID: {product ID: stock}}
 warehouse_df = warehouse_df.set_index(['Warehouse ID', 'Product ID'])
-warehouse_stock = {i: {j: group.at[(i,j), 'Stock'] for j in group.index.get_level_values(1)} for i, group in warehouse_df.groupby(level=0)}
+warehouse_stock = {i: {j: group.at[(i,j), 'Stock'] for j in group.index.get_level_values(1)}
+                                                    for i, group in warehouse_df.groupby(level=0)}
 
 # Make some useful lists
 warehouses = list(warehouse_stock)
@@ -52,17 +54,18 @@ cost = m.addVar(lb=0, vtype=GRB.CONTINUOUS, name='total_cost')
 m.setObjective(cost + 10000*delta_total, sense=GRB.MINIMIZE)
 
 # Define cost variable
-m.addConstr(cost == sum(flow[t]*costs[t[0]][t[1]] for t in indices))
+m.addConstr(cost == sum(flow[t]*costs[t[0]][t[1]]*product_weights[t[2]] for t in indices))
 
 # meet demand
 for o in orders:
     for k in orders_data[o]:
-        m.addConstr(sum(flow[(w, o, k)] for w in warehouses) == orders_data[o][k])
+        m.addConstr(sum(flow[(w, o, k)] for w in warehouses) + order_delta[(o, k)] == orders_data[o][k])
 
 # don't exceed stock
 for w in warehouses:
     for k in products:
-        m.addConstr(sum(flow[(w, o, k)]-order_delta[(o, k)] for o in orders if k in orders_data[o]) <= warehouse_stock[w][k])
+        m.addConstr(sum(flow[(w, o, k)] for o in orders if k in orders_data[o])
+                            <= warehouse_stock[w][k])
 
 # define order delta total
 m.addConstr(delta_total == sum(order_delta[t] for t in deltas_indices))
